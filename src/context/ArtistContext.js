5ffe263b31f8c207fbb3fa589207a07f6248b00f
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-09 14:11:38 +0200

    Revert "Creating functions in artistReducer and in ArtistContext."
    This reverts commit c7f9ec7b201438e156d9a20cdcdce2da08622c3c.

--- src/context/ArtistContext.js
+++ src/context/ArtistContext.js
@@ -1,50 +1,6 @@
-import axios from "axios";
-import { createContext, useEffect } from "react";
+import { createContext } from "react";
 import { initialState } from "../reducer/artistReducer";
 
 const ArtistContext = createContext(initialState);
 
-export const ArtistProvider = ({ childern }) => {
-  const [state, dispatch] = useReducer(artistReducer, initialState);
-
-  useEffect(() => {
-    const getArtist = async (artistId) => {
-      try {
-        const res = await axios.get(`https://run.mocky.io/v3/${artistId}`);
-        dispatch({ type: "GET_ARTIST", payload: res.data });
-      } catch (err) {
-        dispatch({ type: "GET_ARTIST_ERROR", payload: err });
-      }
-    };
-
-    const getArtists = async () => {
-      try {
-        const res = await axios.get(
-          `https://run.mocky.io/v3/3cab6663-7cd8-4365-b8a6-4a1d89305f6a`
-        );
-        dispatch({ type: "GET_ARTISTS", payload: res.data });
-      } catch (err) {
-        dispatch({ type: "GET_ARTISTS_ERROR", payload: err });
-      }
-    };
-
-    getArtist();
-    getArtists();
-  }, []);
-
-  const value = {
-    artist: state.artist,
-    artists: state.artists,
-  };
-  return (
-    <ArtistContext.Provider value={value}>{childern}</ArtistContext.Provider>
-  );
-};
-
-const useArtist = () => {
-  const context = useContext(ArtistContext);
-  if (!context) {
-    throw new Error("useArtist must be used within an ArtistProvider");
-  }
-  return context;
-};
+export default ArtistContext;
