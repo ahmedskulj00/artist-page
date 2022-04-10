cbe8614a44fa725060f193abe7b79a9c129000b6
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-10 12:54:36 +0200

    Adding image to the artist detail page.

--- src/pages/ArtistDetail/ArtistDetail.js
+++ src/pages/ArtistDetail/ArtistDetail.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./ArtistDetail.css";
+import useArtist from "../../context/ArtistContext";
+import { useParams } from "react-router-dom";
 
 const ArtistDetail = () => {
-  return <div>ArtistDetail</div>;
+  const { artist, getArtist } = useArtist();
+  const { artist_uuid } = useParams();
+
+  useEffect(() => {
+    getArtist(artist_uuid);
+  }, [artist_uuid]);
+
+  return (
+    <div className="artistdetail_container">
+      <div className="artistdetail_innercontainer">
+        <div className="artistdetail_image_container">
+          <img
+            src={artist?.data?.image}
+            className="artist_image"
+            alt="artist_image"
+          />
+        </div>
+        <div className="artistdetail_content">
+          <div className="artist_detail"></div>
+          <div className="aritstdetail_mostpopular"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ArtistDetail;
