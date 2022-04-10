a0db2b6aacf71f54b2681a8969341ad192a88c75
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-10 22:39:20 +0200

    Making a PopUpModal component.
    I made this component so I can have reusable modals everywhere I want.

--- src/components/ArtistName/ArtistName.js
+++ src/components/ArtistName/ArtistName.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./ArtistName.css";
 import TrendingIcon from "../../assets/images/icons/hot.svg";
+import PopUpModal from "../PopUpModal/PopUpModal";
 const ArtistName = ({ artist }) => {
   return (
     <>
       <div className="artist_detail_name_container">
         <h1 className="artist_detail_name">{artist?.data?.name}</h1>
-        <div className="subgenre_container"></div>
+        <div className="subgenre_container">
+          <PopUpModal></PopUpModal>
+        </div>
         {artist?.data?.trending === true ? (
           <img
             src={TrendingIcon}
--- src/components/PopUpModal/PopUpModal.js
+++ src/components/PopUpModal/PopUpModal.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./PopUpModal.css";
 
-const PopUpModal = () => {
-  return <div>PopUpModal</div>;
+const PopUpModal = ({ children }) => {
+  return (
+    <div className="popup_container">
+      <div className="popup_inner_container">{children}</div>
+    </div>
+  );
 };
 
 export default PopUpModal;
