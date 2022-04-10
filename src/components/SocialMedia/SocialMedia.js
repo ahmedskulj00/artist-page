8202a793c95c8338c2d2f0223f87b759eef887dd
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-10 16:37:00 +0200

    Importing all icons inside one array of objects for cleaner code.

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./SocialMedia.css";
-
+import Button from "../Button/Button";
+import Plus from "../../assets/images/icons/plus-white.svg";
+import { mockedIcons } from "../../mocked_data/mockedIcons";
 const SocialMedia = ({ artist }) => {
-  return <div>SocialMedia</div>;
+  return (
+    <div className="socialmedia_container">
+      <div className="socialmedia_inner_container">
+        {mockedIcons.map((icon) => (
+          <img src={icon.black} alt={icon.name} key={icon.id} />
+        ))}
+      </div>
+      <Button
+        content={<img src={Plus} width="15px" alt="plus" />}
+        width="30px"
+        height="30px"
+        isPrimary={true}
+      />
+    </div>
+  );
 };
 
 export default SocialMedia;
---
+++ src/mocked_data/mockedIcons.js
@@ -0,0 +1,147 @@
+import HomePageB from "../assets/images/social_icons/icon-link-homepage-black.svg";
+import HomePageC from "../assets/images/social_icons/icon-link-homepage-color.svg";
+import FacebookB from "../assets/images/social_icons/icon-link-facebook-black.svg";
+import FacebookC from "../assets/images/social_icons/icon-link-facebook-color.svg";
+import TwitterB from "../assets/images/social_icons/icon-link-twitter-black.svg";
+import TwitterC from "../assets/images/social_icons/icon-link-twitter-color.svg";
+import SoundCloudB from "../assets/images/social_icons/icon-link-soundcloud-black.svg";
+import SoundCloudC from "../assets/images/social_icons/icon-link-soundcloud-color.svg";
+import YoutubeB from "../assets/images/social_icons/icon-link-youtube-black.svg";
+import YoutubeC from "../assets/images/social_icons/icon-link-youtube-color.svg";
+import BeatPortB from "../assets/images/social_icons/icon-link-beatport-black.svg";
+import BeatPortC from "../assets/images/social_icons/icon-link-beatport-color.svg";
+import MixCloudB from "../assets/images/social_icons/icon-link-mixcloud-black.svg";
+import MixCloudC from "../assets/images/social_icons/icon-link-mixcloud-color.svg";
+import SongKickB from "../assets/images/social_icons/icon-link-songkick-black.svg";
+import SongKickC from "../assets/images/social_icons/icon-link-songkick-color.svg";
+import BandsInTownB from "../assets/images/social_icons/icon-link-bandsintown-black.svg";
+import BandsInTownC from "../assets/images/social_icons/icon-link-bandsintown-color.svg";
+import ITunesB from "../assets/images/social_icons/icon-link-itunes-black.svg";
+import ITunesC from "../assets/images/social_icons/icon-link-itunes-color.svg";
+import InstagramB from "../assets/images/social_icons/icon-link-instagram-black.svg";
+import InstagramC from "../assets/images/social_icons/icon-link-instagram-color.svg";
+import ShazamB from "../assets/images/social_icons/icon-link-shazam-black.svg";
+import ShazamC from "../assets/images/social_icons/icon-link-shazam-color.svg";
+import SpotifyB from "../assets/images/social_icons/icon-link-spotify-black.svg";
+import SpotifyC from "../assets/images/social_icons/icon-link-spotify-color.svg";
+import DeezerB from "../assets/images/social_icons/icon-link-deezer-black.svg";
+import DeezerC from "../assets/images/social_icons/icon-link-deezer-color.svg";
+import TidalB from "../assets/images/social_icons/icon-link-tidal-black.svg";
+import TidalC from "../assets/images/social_icons/icon-link-tidal-color.svg";
+import GooglePlayMusicB from "../assets/images/social_icons/icon-link-googleplaymusic-black.svg";
+import GooglePlayMusicC from "../assets/images/social_icons/icon-link-googleplaymusic-color.svg";
+import NapsterB from "../assets/images/social_icons/icon-link-napster-black.svg";
+import NapsterC from "../assets/images/social_icons/icon-link-napster-color.svg";
+import AmazonMusicB from "../assets/images/social_icons/icon-link-amazonmusic-black.svg";
+import AmazonMusicC from "../assets/images/social_icons/icon-link-amazonmusic-color.svg";
+
+export const mockedIcons = [
+  {
+    id: 0,
+    name: "home_page",
+    black: HomePageB,
+    color: HomePageC,
+  },
+  {
+    id: 1,
+    name: "facebook",
+    black: FacebookB,
+    color: FacebookC,
+  },
+  {
+    id: 2,
+    name: "twitter",
+    black: TwitterB,
+    color: TwitterC,
+  },
+  {
+    id: 3,
+    name: "soundcloud",
+    black: SoundCloudB,
+    color: SoundCloudC,
+  },
+  {
+    id: 4,
+    name: "youtube",
+    black: YoutubeB,
+    color: YoutubeC,
+  },
+  {
+    id: 5,
+    name: "beatport",
+    black: BeatPortB,
+    color: BeatPortC,
+  },
+  {
+    id: 6,
+    name: "mixcloud",
+    black: MixCloudB,
+    color: MixCloudC,
+  },
+  {
+    id: 7,
+    name: "songkick",
+    black: SongKickB,
+    color: SongKickC,
+  },
+  {
+    id: 8,
+    name: "bandsintown",
+    black: BandsInTownB,
+    color: BandsInTownC,
+  },
+  {
+    id: 9,
+    name: "itunes",
+    black: ITunesB,
+    color: ITunesC,
+  },
+  {
+    id: 10,
+    name: "instagram",
+    black: InstagramB,
+    color: InstagramC,
+  },
+  {
+    id: 11,
+    name: "shazam",
+    black: ShazamB,
+    color: ShazamC,
+  },
+  {
+    id: 12,
+    name: "spotify",
+    black: SpotifyB,
+    color: SpotifyC,
+  },
+  {
+    id: 13,
+    name: "deezer",
+    black: DeezerB,
+    color: DeezerC,
+  },
+  {
+    id: 14,
+    name: "tidal",
+    black: TidalB,
+    color: TidalC,
+  },
+  {
+    id: 15,
+    name: "google_play_music",
+    black: GooglePlayMusicB,
+    color: GooglePlayMusicC,
+  },
+  {
+    id: 16,
+    name: "napster",
+    black: NapsterB,
+    color: NapsterC,
+  },
+  {
+    id: 17,
+    name: "amazon_music",
+    black: AmazonMusicB,
+    color: AmazonMusicC,
+  },
+];
