1b227b26dae208e4ce957426ba632fa28637d8fe
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-10 16:59:31 +0200

    Implemented the dynamic and clean way of importing all the icons/links and changing the color of the icons on the hover.
    Using .map functionality I was first mapping through all the links inside api data, and then with ternary operator I checked if the name from mockedIcon array is equal to the name inside the api data, and only then show the icon, with this I'm able to show only the icons that are needed for certain artist.
    
    Also with the combination of useState and onMouseOver function, I was able to change the color of the icon, by checking if the hover link is being hovered, and if it is change its color.

--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SocialMedia.css";
 import Button from "../Button/Button";
 import Plus from "../../assets/images/icons/plus-white.svg";
 import { mockedIcons } from "../../mocked_data/mockedIcons";
+
 const SocialMedia = ({ artist }) => {
+  const [color, setColor] = useState();
+
   return (
     <div className="socialmedia_container">
       <div className="socialmedia_inner_container">
-        {mockedIcons.map((icon) => (
-          <img src={icon.black} alt={icon.name} key={icon.id} />
+        {artist?.data?.social_links.map((social_link, index) => (
+          <a
+            href={social_link.link}
+            key={index}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {mockedIcons.map((icon, index) =>
+              icon.name === social_link.channel ? (
+                <img
+                  src={color === index ? icon.color : icon.black}
+                  alt="icon"
+                  key={index}
+                  onMouseOver={() => setColor(index)}
+                  onMouseOut={() => setColor(null)}
+                />
+              ) : null
+            )}
+          </a>
         ))}
       </div>
       <Button
