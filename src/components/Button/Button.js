08da92fcc4ef633ddffaa441a148dbe87247277e
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-10 14:11:34 +0200

    Added buttons in the "follow" section, along with the zoom properties.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Button.css";
 
-const Button = ({ width, height, content, onClick, isPrimary }) => {
+const Button = ({ width, height, content, onClick, isPrimary, id }) => {
   return (
     <>
       <button
         className={isPrimary ? "btn primary_button" : "btn secondary_button"}
         style={{ width: width, height: height }}
         onClick={onClick}
+        id={id}
       >
         {content}
       </button>
--- src/pages/ArtistDetail/ArtistDetail.js
+++ src/pages/ArtistDetail/ArtistDetail.js
@@ -4,6 +4,8 @@ import useArtist from "../../context/ArtistContext";
 import { useParams } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import TrendingIcon from "../../assets/images/icons/hot.svg";
+import Heart from "../../assets/images/icons/heart-white.svg";
+import ShareWhite from "../../assets/images/icons/share-white.svg";
 const ArtistDetail = () => {
   const { artist, getArtist } = useArtist();
   const { artist_uuid } = useParams();
@@ -36,6 +38,43 @@ const ArtistDetail = () => {
                     className="trending_icon"
                   />
                 ) : null}
+              </div>
+              <div className="buttons_container">
+                <div className="follow_button_container">
+                  <Button
+                    content={
+                      <img
+                        src={Heart}
+                        width="20px"
+                        alt="heart_icon"
+                        className="heart_icon"
+                      />
+                    }
+                    width="36px"
+                    height="36px"
+                    isPrimary={true}
+                  />
+                  <Button
+                    content="Follow"
+                    isPrimary={true}
+                    width="180px"
+                    height="36px"
+                  />
+                </div>
+                <Button
+                  content={
+                    <img
+                      src={ShareWhite}
+                      width="20px"
+                      alt="share_icon"
+                      className="share_icon"
+                    />
+                  }
+                  width="36px"
+                  height="36px"
+                  isPrimary={true}
+                  id="share_button"
+                />
               </div>
             </div>
           </div>
