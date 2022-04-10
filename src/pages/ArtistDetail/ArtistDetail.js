e2fbbebbc9426a2e450dc8dfcee37fba6d49b2be
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-10 13:45:50 +0200

    Started working on ArtistDetail content part of the page.

--- src/pages/ArtistDetail/ArtistDetail.js
+++ src/pages/ArtistDetail/ArtistDetail.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import "./ArtistDetail.css";
 import useArtist from "../../context/ArtistContext";
 import { useParams } from "react-router-dom";
-
+import Button from "../../components/Button/Button";
+import TrendingIcon from "../../assets/images/icons/hot.svg";
 const ArtistDetail = () => {
   const { artist, getArtist } = useArtist();
   const { artist_uuid } = useParams();
@@ -23,7 +24,20 @@ const ArtistDetail = () => {
         </div>
         <div className="artistdetail_content">
           <div className="artist_detail">
-            <div className="artist_detail_inner"></div>
+            <div className="artist_detail_inner">
+              <Button content="Booking request" width="217px" height="37px" />
+              <div className="artist_detail_name_container">
+                <h1 className="artist_detail_name">{artist?.data?.name}</h1>
+                <div className="subgenre_container"></div>
+                {artist?.data?.trending === true ? (
+                  <img
+                    src={TrendingIcon}
+                    alt="trending_icon"
+                    className="trending_icon"
+                  />
+                ) : null}
+              </div>
+            </div>
           </div>
           <div className="artistdetail_mostpopular"></div>
         </div>
