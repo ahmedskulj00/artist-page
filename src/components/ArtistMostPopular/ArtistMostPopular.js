734e4fde57b1db1ecc17197b0bff00b819c8b0ae
Ahmed Škulj <66512187+ahmedskulj00@users.noreply.github.com>
2022-04-11 11:14:27 +0200

    Implemented the most popular in chart.

--- src/components/ArtistMostPopular/ArtistMostPopular.js
+++ src/components/ArtistMostPopular/ArtistMostPopular.js
@@ -1,8 +1,55 @@
 import React from "react";
 import "./ArtistMostPopular.css";
+import { ResponsiveBar } from "@nivo/bar";
 
 const ArtistMostPopular = ({ artist }) => {
-  return <div className="mostpopular_container"></div>;
+  const data = [
+    {
+      city: artist?.data?.popularity[0].city,
+      percentage: artist?.data?.popularity[0].percentage,
+    },
+    {
+      city: artist?.data?.popularity[1].city,
+      percentage: artist?.data?.popularity[1].percentage,
+    },
+    {
+      city: artist?.data?.popularity[2].city,
+      percentage: artist?.data?.popularity[2].percentage,
+    },
+    {
+      city: artist?.data?.popularity[3].city,
+      percentage: artist?.data?.popularity[3].percentage,
+    },
+    {
+      city: artist?.data?.popularity[4].city,
+      percentage: artist?.data?.popularity[4].percentage,
+    },
+  ];
+
+  return (
+    <div className="mostpopular_container">
+      <div className="mostpopular_title">
+        <h5>MOST POPULAR IN</h5>
+      </div>
+      <ResponsiveBar
+        data={data}
+        keys={["percentage"]}
+        indexBy="city"
+        margin={{ top: 60, right: 0, bottom: 50, left: 30 }}
+        padding={0.8}
+        colors="#000"
+        layout="horizontal"
+        axisBottom={null}
+        axisTop={null}
+        axisLeft={{
+          tickSize: 0,
+          tickPadding: 5,
+        }}
+        enableLabel={false}
+        isInteractive={false}
+      />
+    </div>
+  );
 };
 
 export default ArtistMostPopular;
